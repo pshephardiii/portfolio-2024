fdefc9f587559a11c48ea27f983ecce4bb2d35dc
Paul Shephard <paulshephard598@gmail.com>
2024-05-06 12:39:41 -0400

    set up routes for router

---
+++ src/router/routes.js
@@ -0,0 +1,29 @@
+import HomePage from '../pages/HomePage/HomePage'
+import AboutPage from '../pages/AboutPage/AboutPage'
+import ProjectsPage from '../pages/ProjectsPage/ProjectsPage'
+import ContactPage from '../pages.ContactPage/ContactPage'
+
+const routes = [
+    {
+        Component: HomePage,
+        key: 'Home',
+        path: '/home'
+    },
+    {
+        Component: AboutPage,
+        key: 'About',
+        path: '/about'
+    },
+    {
+        Component: ProjectsPage,
+        key: 'Projects',
+        path: '/projects'
+    },
+    {
+        Component: ContactPage,
+        key: 'Contact',
+        path: '/contact'
+    }
+]
+
+export default routes
