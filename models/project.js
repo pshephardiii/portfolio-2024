65b822f8cc648a681c2578c09c7c3006bbf75bb1
Paul Shephard <paulshephard598@gmail.com>
2024-05-07 11:22:50 -0400

    seeded in projects

--- models/project.js
+++ models/project.js
@@ -2,10 +2,12 @@ const { model, Schema } = require('mongoose')
 
 const projectSchema = new Schema ({
     name: { required: true, type: String },
-    image: { required: true, type: String} ,
+    summary: { required: true, type: String },
+    tools: {required: true, type: String },
+    image: { required: true, type: String },
     description: { required: true, type: String },
-    githubLink: {required: true, type: String },
-    liveProjectLink: {required: true, type: String }
+    githubLink: { required: true, type: String },
+    liveProjectLink: { required: true, type: String }
 }, {
     timestamps: true
 })
